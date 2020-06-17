0c67c70dcde927f2b02fcda557d68246824bbd87
Edands <theedands@gmail.com>
2020-06-17 11:55:15 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+TDDO: 
+    Here's what the app should do:
+
+    When the user first loads the page, the list of to-do items should appear on the page, matching what's in the "database".
+
+    When someone clicks the "add record" button, a new record should be added to the HTML list using default (hard-coded or slightly randomized) information.
+    A new record should also be added to the "database".
+
+    When someone clicks the "delete" button next to a record, that record will removed from the list of items in the HTML. 
+    That record should also be removed from the "database".
+
+    When someone clicks the "edit" button next to a record, a form should appear, populated with the existing information for that record.
+
+    When that form is saved, the HTML and the "database" of the appropriate record with the edited information shoud be updated.
+ */
