f6f26005b3364a737c410bb69c9d77c19a3277a0
Edands <theedands@gmail.com>
2020-06-19 04:53:09 -0400

    save functions

--- script.js
+++ script.js
@@ -19,33 +19,48 @@ var recordTitle = document.getElementById("record-title");
 var recordSave = document.getElementById("save-new-btn");
 var recordNew = document.getElementById("make-new-btn");
 
+// Generates a random unique id for new records
+const uid = function () {
+	return Date.now().toString(36) + Math.random().toString(36).substr(2);
+};
+
+// Gets the current date in the format MM/DD/YY , HH:MM:SS
 function curentDate() {
 	return new Date().toLocaleString();
 }
 
+// Shows the input field for new records
 function makeNewRecord() {
 	recordTitle.style.setProperty("display", "block");
 	recordSave.style.setProperty("display", "block");
 	recordNew.style.setProperty("display", "none");
 }
 
+// TODO: Function to save locally the records
+
+// Gets the input text, clones a hidden template and appends it to stored-records with a unique id
 function saveNewRecord() {
+	// Gets the inptup value and clones hidden template
 	let input = document.getElementById("record-title").value;
 	let recordTemplate = document.getElementById("record00");
 	let recordClone = recordTemplate.cloneNode(true);
 	let storedRecordsDiv = document.getElementById("stored-records");
-	// TODO: Get a different id
 	storedRecordsDiv.appendChild(recordClone);
 	let newRecord = storedRecordsDiv.lastChild;
-	// TODO: Change info in newRecord
+
+	//  Change info in newRecord
 	newRecord.querySelector(".title").innerHTML = `${input}`;
 	newRecord.querySelector(".creation-date").innerHTML = curentDate();
+	newRecord.id = uid();
 	newRecord.style.setProperty("display", "block");
 
+	// Return input field to its original state
 	recordTitle.style.setProperty("display", "none");
 	recordSave.style.setProperty("display", "none");
 	recordNew.style.setProperty("display", "block");
 }
+
+// Shows the edit record form
 
 function editRecord(caller) {
 	let parentElement = caller.parentNode.id;
@@ -59,17 +74,27 @@ function editRecord(caller) {
 	}
 }
 
+// Removes the record TODO: make a confirmation popup
+
 function removeRecord(caller) {
 	let parentElement = caller.parentNode.id;
 	document.getElementById(parentElement).remove();
 	console.log(`Record ${parentElement} removed`);
 }
 
+// Save the edits into the form TODO: Integrate with localStorage and refactor
+
 function saveForm(caller) {
 	let parentElement = caller.parentNode.parentNode.id;
 	let editForm = document
 		.getElementById(parentElement)
 		.getElementsByClassName("edit-form")[0];
+	let editInput = editForm.getElementsByClassName("edit-input")[0].value;
+
+	document
+		.getElementById(parentElement)
+		.getElementsByClassName("title")[0].innerHTML = editInput;
+
 	if (editForm.style["display"] == "none") {
 		editForm.style["display"] = "block";
 	} else {
